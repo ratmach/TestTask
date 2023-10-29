51f91886a3335a06869ce4f1c29e3650365eeb41
ratmach <ratmach@yahoo.com>
2023-10-29 05:27:01 +0400

    updated docker added `/operators` endpoint

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from fastapi import FastAPI
 
-from sum import operators, OperationRequest
+from sum import operators, OperationRequest, registered_operators_as_enum
 
 app = FastAPI()
 
 
 @app.post("/calculate")
-async def calculate(data: OperationRequest):
+def calculate(data: OperationRequest):
     result = operators(data.operator)(data)
     return result.resultDict()
+
+
+@app.get("/operators")
+def get_supported_operators():
+    return dict(operators=sorted([i.value for i in registered_operators_as_enum()]))
 
 
 if __name__ == "__main__":
--- sum/__init__.py
+++ sum/__init__.py
@@ -1,4 +1,4 @@
-from .Operation import operators
+from .Operation import operators, registered_operators_as_enum
 from .Operations import *
 
 from .OperationRequest import OperationRequest
