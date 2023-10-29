af121d59e4ce08fca41f6e365848aecad46159a9
ratmach <ratmach@yahoo.com>
2023-10-29 07:40:58 +0400

    merged styles

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,5 @@ RUN pip install -r requirements.txt
 COPY . /backend
 
 EXPOSE 8000
-ENV APP_PREFIX = "/api"
 
 CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
--- FE/calculator/src/App.tsx
+++ FE/calculator/src/App.tsx
@@ -1,5 +1,4 @@
 import React, {useEffect, useState} from 'react';
-import './App.scss';
 import {getSupportedOperators, postCalculate} from "./service/api";
 import {postCalculateRequest} from "./service/requests";
 
