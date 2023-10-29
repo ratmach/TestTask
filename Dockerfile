3ea3c853f2228263539bd237b56b4969f264e450
ratmach <ratmach@yahoo.com>
2023-10-29 07:22:37 +0400

    updated root

--- Dockerfile
+++ Dockerfile
@@ -8,5 +8,6 @@ RUN pip install -r requirements.txt
 COPY . /backend
 
 EXPOSE 8000
+ENV APP_PREFIX = "/api"
 
 CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
--- main.py
+++ main.py
@@ -1,20 +1,25 @@
-from fastapi import FastAPI
+import os
+
+from fastapi import FastAPI, APIRouter
 
 from sum import operators, OperationRequest, registered_operators_as_enum
 
 app = FastAPI()
+router = APIRouter()
 
 
-@app.post("/calculate")
+@router.post("/calculate")
 def calculate(data: OperationRequest):
     result = operators(data.operator)(data)
     return result.resultDict()
 
 
-@app.get("/operators")
+@router.get("/operators")
 def get_supported_operators():
     return dict(operators=sorted([i.value for i in registered_operators_as_enum()]))
 
+
+app.include_router(router, prefix=os.getenv("APP_PREFIX", ""))
 
 if __name__ == "__main__":
     import uvicorn
